ca13b55fb62a866c965a22eb362c34ae9ab303bf
Pierre-Claver KOKO BANYWERHA <pierreclaverkoko.web@gmail.com>
2017-03-07 12:32:48 +0200

    Adding the main() function in main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # application Instanciation
 
 #from pharmacie import Application
+def main():
+	"""Main function for boostraping application """
+	#Values from config.json
+	PROJECT_NAME = ""
+	AUTHOR = ""
+	VERSION = ""
+	LICENSE = ""
 
-#if "__class__" == "__name__":
-#	Application()
+if "__class__" == "__name__":
+	main()
